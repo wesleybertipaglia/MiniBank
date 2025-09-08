9b4469f2da6001fa9f25bc5baa1ceef40c647e28
wesleybertipaglia <wesleybertipaglia@gmail.com>
2025-09-08 18:47:11 -0300

    feat (auth): add user service

---
+++ MiniBank.Auth/MiniBank.Auth.Application/Service/UserService.cs
@@ -0,0 +1,38 @@
+using MiniBank.Auth.Core.Dto;
+using MiniBank.Auth.Core.Interface;
+using MiniBank.Auth.Core.Mapper;
+
+namespace MiniBank.Auth.Application.Service;
+
+public class UserService(IUserRepository userRepository) : IUserService
+{
+    public async Task<UserDto> GetByIdAsync(Guid id)
+    {
+        var user = await userRepository.GetByIdAsync(id) 
+                   ?? throw new Exception($"User with ID '{id}' not found.");
+
+        return UserMapper.Map(user);
+    }
+
+    public async Task<UserDto> GetByEmailAsync(string email)
+    {
+        var user = await userRepository.GetByEmailAsync(email) 
+                   ?? throw new Exception($"User with email '{email}' not found.");
+
+        return UserMapper.Map(user);
+    }
+
+    public async Task<UserDto> ConfirmEmailAsync(Guid userId)
+    {
+        var user = await userRepository.GetByIdAsync(userId) 
+                   ?? throw new Exception($"User with ID '{userId}' not found.");
+
+        if (user.EmailConfirmed)
+            throw new InvalidOperationException("Email already confirmed.");
+
+        user.EmailConfirmed = true;
+        await userRepository.UpdateAsync(user);
+
+        return UserMapper.Map(user);
+    }
+}
--- MiniBank.Auth/MiniBank.Auth.Core/Interface/IUserService.cs
+++ MiniBank.Auth/MiniBank.Auth.Core/Interface/IUserService.cs
@@ -1,6 +1,10 @@
+using MiniBank.Auth.Core.Dto;
+
 namespace MiniBank.Auth.Core.Interface;
 
-public class IUserService
+public interface IUserService
 {
-    
+    Task<UserDto> GetByIdAsync(Guid id);
+    Task<UserDto> GetByEmailAsync(string email);
+    Task<UserDto> ConfirmEmailAsync(Guid userId);
 }
