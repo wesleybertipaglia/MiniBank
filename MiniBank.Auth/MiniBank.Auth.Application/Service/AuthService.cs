bb32c203aa3cfc8661489512a2e67d068e266654
wesleybertipaglia <wesleybertipaglia@gmail.com>
2025-09-08 18:55:22 -0300

    feat (auth): add token helper

--- MiniBank.Auth/MiniBank.Auth.Application/Service/AuthService.cs
+++ MiniBank.Auth/MiniBank.Auth.Application/Service/AuthService.cs
@@ -1,12 +1,8 @@
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
 using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
 using MiniBank.Auth.Core.Dto;
+using MiniBank.Auth.Core.Helpers;
 using MiniBank.Auth.Core.Interface;
 using MiniBank.Auth.Core.Mapper;
-using MiniBank.Auth.Core.Model;
 
 namespace MiniBank.Auth.Application.Service;
 
@@ -22,7 +18,7 @@ public class AuthService(IUserRepository userRepository, IConfiguration configur
             throw new UnauthorizedAccessException("Invalid email or password");
         }
         
-        var token = GenerateJwtToken(user);
+        var token = TokenHelper.GenerateJwtToken(configuration, user);
 
         return AuthMapper.Map
         (
@@ -41,35 +37,12 @@ public class AuthService(IUserRepository userRepository, IConfiguration configur
         
         var user = UserMapper.Map(signUpRequestDto);
         await userRepository.CreateAsync(user);
-        var token = GenerateJwtToken(user);
+        var token = TokenHelper.GenerateJwtToken(configuration, user);
         
         return AuthMapper.Map
         (
             UserMapper.Map(user),
             TokenMapper.Map(token)
         );
-    }
-
-    private string GenerateJwtToken(User user)
-    {
-        var claims = new[]
-        {
-            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-            new Claim(ClaimTypes.Name, user.Name),
-            new Claim(ClaimTypes.Email, user.Email)
-        };
-        
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:SecretKey"]));
-        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-
-        var token = new JwtSecurityToken(
-            issuer: configuration["Jwt:Issuer"],
-            audience: configuration["Jwt:Audience"],
-            claims: claims,
-            expires: DateTime.Now.AddHours(1),
-            signingCredentials: credentials
-        );
-
-        return new JwtSecurityTokenHandler().WriteToken(token);
     }
 }
